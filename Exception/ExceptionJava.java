5ddff8cdcb514bc3c5cb245006cbeeb6ece7fa12
linhfishhh <linhfish2309@gmail.com>
2021-06-25 16:44:34 +0700

    f

---
+++ Exception/ExceptionJava.java
@@ -0,0 +1,27 @@
+package Exception;
+import java.util.Random;
+import java.util.Scanner;
+public class ExceptionJava {
+	public Integer[] createRandom() {
+		Random rd=new Random();
+		Integer[] arr=new Integer[100];
+		System.out.println("Danh sach phan tu cua mang");
+		for (int i=0;i<100;i++) {
+			arr[i]=rd.nextInt(100);
+		}
+		return arr;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExceptionJava arrExample= new ExceptionJava();
+		Integer[] arr=arrExample.createRandom();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+		int x = sc.nextInt();
+		try {
+			System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + arr[x]);
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("Chỉ số vượt quá giới hạn của mảng");
+		}
+	}
+}
--- insertsort1/InsertSort.java
+++ insertsort1/InsertSort.java
@@ -2,20 +2,19 @@ package insertsort1;
 
 public class InsertSort {
 	public static void insertSort(int[] arr) {
-		int pos,x;
+		int index;
 		for(int i=1;i<arr.length;i++) {
-			x=arr[i];
-			pos=i;
-			while(pos>=0 && x<arr[pos-1]) {
-				arr[pos]=arr[pos-1];
-				pos--;
+			index=arr[i];
+			while(i>0 && index<arr[i-1]) {
+				arr[i]=arr[i-1];
+				i--;
 			}
-			arr[pos]=x;
+			arr[i]=index;
 		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr= {1,3,5,6,7,2};
+		int[] arr= {1,3,5,2};
 		InsertSort.insertSort(arr);
 		for(int i=0;i<arr.length;i++) {
 			System.out.println(arr[i]+" ");
